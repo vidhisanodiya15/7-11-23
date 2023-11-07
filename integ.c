eac0e53aa661910382dab404282068281df90beb
Vidhi sanodiya <114800338+vidhisanodiya15@users.noreply.github.com>
2023-11-07 23:16:02 +0530

    Add files via upload

---
+++ integ.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a=6543;
+	printf("*%5d,%-5d*",a,a);
+	return 0;
+}
+
+int main(){
+	int b=123;
+	printf("*%06d",b)
+	return 0;
+}
---
+++ intro.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+// "c" intro
+int main()
+{
+	printf("Hello lpu");
+	return 0;
+}
